9b66156e6164ac68158204c2e3120acb9b043002
Anifyuli <anifyuliansyah@gmail.com>
2024-04-26 20:55:26 +0700

    edit(programs/programs.nix): Erase weird whitespace

--- programs/programs.nix
+++ programs/programs.nix
@@ -58,7 +58,7 @@
 
   # Enable ADB 
   programs.adb.enable = true;
-
+ 
   # Some programs need SUID wrappers, can be configured further or are started in user sessions.
   programs.mtr.enable = true;
   programs.gnupg.agent = {
