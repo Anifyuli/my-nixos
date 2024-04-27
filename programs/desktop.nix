a89df3a2a2437d82ad34b3a6a663e9dad870515a
Anifyuli <anifyuliansyah@gmail.com>
2024-04-27 07:40:31 +0700

    fix(desktop.nix): Fix disabled tap-to-click issue on GDM

--- programs/desktop.nix
+++ programs/desktop.nix
@@ -25,6 +25,12 @@
       vistafonts
     ];
   };
+
+  # Override default Dconf settings.
+  services.xserver.desktopManager.gnome.extraGSettingsOverrides = "
+    [org.gnome.desktop.peripherals.touchpad]
+    tap-to-click=true
+  ";
   
   # Enable ls colors in Bash
   programs.bash.enableLsColors = true;
