d6e897773fb7462f5a4bb2e4dd9e9e0233d1dae2
Anifyuli <anifyuliansyah@gmail.com>
2024-06-26 20:12:33 +0700

    edit(home.nix): Add Bash & fastfetch configs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -12,7 +12,7 @@
   home.packages = with pkgs; [
     alacritty-theme
     exercism
-    gnomeExtensions.cloudflare-warp-toggle
+    gnomeExtensions.cloudflare-warp-indicator
     gnomeExtensions.x11-gestures
     mongodb-compass
     pnpm
@@ -37,23 +37,147 @@
       "?"
       "??"
     ];
-    historyFile = "~/.bash_history";
+    historyFile = "$HOME/.bash_history";
+    historyFileSize = 1000000;
+    historySize = 1000;
     shellOptions = [
       "autocd"
       "cdspell"
+      "checkjobs"
       "direxpand"
       "dirspell" 
+      "extglob"
       "globstar" 
       "histappend" 
       "histverify"
       "nocaseglob" 
       "no_empty_cmd_completion"
     ];
-    bashrcExtra = "
-      source ~/.bash/themes/aphrodite/aphrodite.theme.sh # Use Aphrodite prompt theme
-    ";
+    bashrcExtra = "source ~/.bash/themes/aphrodite/aphrodite.theme.sh # Use Aphrodite prompt theme";
   };
- 
+
+  # dircolors configurations
+  programs.dircolors = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+  
+  # Fastfetch configurations
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      schema = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+      display = {
+        separator = " ";
+      };
+      modules = [
+      {
+        type = "host";
+        key = "╭─󰌢";
+        keyColor = "green";
+      }
+      {
+        type = "cpu";
+        key = "├─󰻠";
+        keyColor = "green";
+      }
+      {
+        type = "gpu";
+        key = "├─󰍛";
+        keyColor = "green";
+      }
+      {
+        type = "disk";
+        key = "├─";
+        keyColor = "green";
+      }
+      {
+        type = "memory";
+        key = "├─󰑭";
+        keyColor = "green";
+      }
+      {
+        type = "swap";
+        key = "├─󰓡";
+        keyColor = "green";
+      }
+      {
+        type = "display";
+        key = "╰─󰍹";
+        keyColor = "green";
+      }
+
+      "break"
+
+      {
+        type = "shell";
+        key = "╭─";
+        keyColor = "yellow";
+      }
+      {
+        type = "terminal";
+        key = "├─";
+        keyColor = "yellow";
+      }
+      {
+        type = "terminalfont";
+        key = "├─";
+        keyColor = "yellow";
+      }
+      {
+        type = "de";
+        key = "├─";
+        keyColor = "yellow";
+      }
+      {
+        type = "wm";
+        key = "├─";
+        keyColor = "yellow";
+      }
+      {
+        type = "theme";
+        key = "├─󰉼";
+        keyColor = "yellow";
+      }
+      {
+        type = "icons";
+        key = "╰─󰀻";
+        keyColor = "yellow";
+      }
+
+      "break"
+
+      {
+        type = "title";
+        key = "╭─";
+        format = "{1}@{2}";
+        keyColor = "blue";
+      }
+      {
+        type = "os";
+        key = "├─"; # Just get your distro's logo off nerdfonts.com
+          keyColor = "blue";
+      }
+      {
+        type = "kernel";
+        key = "├─";
+        format = "{1} {2}";
+        keyColor = "blue";
+      }
+      {
+        type = "packages";
+        key = "├─󰏖";
+        keyColor = "blue";
+      }
+      {
+        type = "uptime";
+        key = "╰─󰅐";
+        keyColor = "blue";
+      }
+      ];
+    };
+  };
+
   # Golang configurations.
   programs.go = {
     enable = true;
