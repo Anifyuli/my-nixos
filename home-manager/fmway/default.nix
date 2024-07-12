78550a73736f3532ca49c261e9cf341c153045de
fmway <fm18lv@gmail.com>
2024-07-12 20:00:44 +0700

    (nothing)

--- home-manager/fmway/default.nix
+++ home-manager/fmway/default.nix
@@ -100,7 +100,7 @@ in {
       extraConfig = {
         hub.protocol = "ssh";
         credential.helper = "${
-          pkgs.git
+          pkgs.git-secret
         }/bin/git-credential-libsecret";
       };
     };
