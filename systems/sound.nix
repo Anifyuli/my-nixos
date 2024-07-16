38f4d299254bc55d86833982baa9549dc9dee15b
fmway <fm18lv@gmail.com>
2024-07-16 12:49:22 +0700

    (config:fix) migration option in pipewire

--- systems/sound.nix
+++ systems/sound.nix
@@ -1,12 +1,17 @@
 {
   # pipewire
   security.rtkit.enable = true;
+
   services.pipewire = {
     enable = true;
+    audio.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     # jack.enable = true;
   };
+
+  # disable pulseaudio
+  hardware.pulseaudio.enable = false;
 }
--- systems/xserver.nix
+++ systems/xserver.nix
@@ -17,11 +17,6 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
 }
