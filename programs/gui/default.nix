a91f56a2f1be696c6018f8ad79fa1a80bb5c23f2
fmway <fm18lv@gmail.com>
2024-07-16 20:43:18 +0700

    (nothing) ...

--- programs/gui/default.nix
+++ programs/gui/default.nix
@@ -18,25 +18,4 @@
     mako
     firefoxpwa
   ];
-  programs = {
-    # firefox
-    firefox = {
-      enable = true;
-      package = pkgs.firefox;
-      nativeMessagingHosts.packages = [pkgs.firefoxpwa];
-      wrapperConfig = {
-        pipewireSupport = true;
-      };
-    };
-
-    # Steam
-    steam = {
-      enable = true;
-      #remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      #dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-      gamescopeSession.enable = true;
-    };
-
-  };
-
 }
