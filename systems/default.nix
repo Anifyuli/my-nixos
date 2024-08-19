2c371ee92a025fada1edd8f22c7a35995b821b4e
fmway <fm18lv@gmail.com>
2024-08-19 08:58:59 +0700

    (config:fix) move data to single file

--- systems/default.nix
+++ systems/default.nix
@@ -8,13 +8,6 @@
 
 treeImport {
   imports = genTreeImports ./extra;
-
-  data = {
-    list-users = let
-      inherit (config.home-manager) users;
-      inherit (builtins) attrNames;
-    in map (x: users.${x}.home.username) (attrNames users); # i use home-manager.users instead of users.users, because i don't want ribet pake filter segala
-  };
 }
 {
   excludes = [
