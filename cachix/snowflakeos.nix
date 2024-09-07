ba158411f6b99a2f40b5c83c705109afac197196
fmway <fm18lv@gmail.com>
2024-09-07 18:08:26 +0700

    (cachix:add) snowflakeos

---
+++ cachix/snowflakeos.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://snowflakeos.cachix.org"
+      ];
+      trusted-public-keys = [
+        "snowflakeos.cachix.org-1:gXb32BL86r9bw1kBiw9AJuIkqN49xBvPd1ZW8YlqO70="
+      ];
+    };
+  };
+}
