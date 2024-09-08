fa49995273a6b9341e43d64cc7e6d478f7603ed0
fmway <fm18lv@gmail.com>
2024-09-08 12:36:07 +0700

    (home-manager:add) some fish abbrs

--- home-manager/fmway/programs/fish/interactiveShellInit.fish
+++ home-manager/fmway/programs/fish/interactiveShellInit.fish
@@ -1,2 +1,7 @@
+abbr -a nofu "$(string join \n -- 'cd /etc/nixos' 'doas nix flake update' 'cd -')"
+abbr -a nofl "$(string join \n -- 'cd /etc/nixos' 'doas nix flake lock' 'cd -')"
+abbr -a noflu \
+  --set-cursor=! \
+  "$(string join \n -- 'cd /etc/nixos' 'doas nix flake lock --update-input !' 'cd -')"
 set fish_greeting # Disable greeting
 printf '\e[5 q'
