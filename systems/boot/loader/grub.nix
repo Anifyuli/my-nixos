5dd400a55e2b67e9c5ed78df25a57b19aee03e2b
fmway <fm18lv@gmail.com>
2024-09-08 12:56:22 +0700

    (nothing) ...

--- systems/boot/loader/grub.nix
+++ systems/boot/loader/grub.nix
@@ -6,7 +6,7 @@
   efiSupport = true;
   fsIdentifier = "label";
   zfsSupport = true;
-  device = "/dev/disk/by-id/nvme-KINGSTON_OM8PDP3256B-AB1_50026B768583ADF8-part1";
+  # device = "/dev/disk/by-id/nvme-KINGSTON_OM8PDP3256B-AB1_50026B768583ADF8-part1";
   mirroredBoots = [
     { devices = [ "nodev" ]; path = "/boot"; }
   ];
