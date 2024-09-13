84ed8c5aba9124da1e0a8c401b8efd3cf241e60b
fmway <fm18lv@gmail.com>
2024-09-13 18:00:15 +0700

    (config:fix) samba move prefix

--- systems/services/samba/default.nix
+++ systems/services/samba/default.nix
@@ -2,6 +2,19 @@
 {
   enable = false;
   package = pkgs.sambaFull;
+  settings.global = {
+    "workgroup" = "WORKGROUP";
+    "server string" = "smbnix";
+    "netbios name" = "smbnix";
+    "security" = "user ";
+    # "use sendfile" = "yes";
+    # "max protocol" = "smb2";
+    # note: localhost is the ipv6 localhost ::1
+    "hosts allow" = "192.168. 127.0.0.1 localhost";
+    # "hosts deny" = "0.0.0.0/0";
+    "guest account" = "nobody";
+    "map to guest" = "bad user";
+  };
   securityType = "user";
   openFirewall = true;
 }
