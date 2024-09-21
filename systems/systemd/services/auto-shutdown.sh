a373b36d46fd76918ff0e20d0be9717ae7ef63f1
fmway <fm18lv@gmail.com>
2024-09-21 07:18:33 +0700

    (config:fix) auto-shutdown

--- systems/systemd/services/auto-shutdown.sh
+++ systems/systemd/services/auto-shutdown.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/bash
-
 # reference: https://bbs.archlinux.org/viewtopic.php?pid=1280941#p1280941
 
 BATTERY_CAPACITY=$(cat /sys/class/power_supply/BAT0/capacity)
@@ -8,16 +6,17 @@ BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
 NOTIFY_TITLE="Baterai sekarat"
 NOTIFY_ICON=battery_empty
 NOTIFY_MESSAGE="Mati sia anjing!!!"
+NOTIFY_SEND=$(command -v notify-send)
 
 SHUTDOWN_WITH=${SHUTDOWN_WITH:-5}
 
 WM=gnome-shell
 WMPID=$(pidof ${WM})
-WMUSER=$(ps -e -o user,comm | grep ${WM} | cut -f1 -d " ")
-DBUS=$(egrep -z 'DBUS_SESSION_BUS_ADDRESS|DISPLAY' /proc/${WMPID}/environ | sed -r -e 's/(.)DBUS_/\1 DBUS_/' -e 's/(.)DISPLAY/\1 DISPLAY/')
+WMUSER=$(ps -e -o pid,user,comm | grep ${WMPID} | awk '{print $2}')
+DBUS=$(cat /proc/${WMPID}/environ | tr \0 \n | grep 'NOTIFY_SOCKET')
 
 if [[ $BATTERY_CAPACITY -le $SHUTDOWN_WITH && $BATTERY_STATUS = "Discharging" ]]; then
-	/bin/su ${WMUSER} -c "${DBUS} notify-send --urgency=critical --hint=int:transient:1 --icon $NOTIFY_ICON '$NOTIFY_TITLE' '$NOTIFY_MESSAGE'"
+	/bin/su ${WMUSER} -c "${DBUS} ${NOTIFY_SEND} --urgency=critical --hint=int:transient:1 --icon $NOTIFY_ICON '$NOTIFY_TITLE' '$NOTIFY_MESSAGE'"
 	sleep 60s
 	BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
 	if [[ $BATTERY_STATUS = "Discharging" ]]; then
