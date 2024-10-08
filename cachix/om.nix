18210be826dfd68f74cee403ce115dab9363654c
fmway <fm18lv@gmail.com>
2024-10-09 00:15:58 +0700

    (cachix:add) omnix

---
+++ cachix/om.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://om.cachix.org"
+      ];
+      trusted-public-keys = [
+        "om.cachix.org-1:ifal/RLZJKN4sbpScyPGqJ2+appCslzu7ZZF/C01f2Q="
+      ];
+    };
+  };
+}
