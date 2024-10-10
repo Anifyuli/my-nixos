7b5143b9d4ec768d29951bec2934c3b9adb56b51
fmway <fm18lv@gmail.com>
2024-10-10 21:06:27 +0700

    (cachix:add) tweag-jupyter

---
+++ cachix/tweag-jupyter.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://tweag-jupyter.cachix.org"
+      ];
+      trusted-public-keys = [
+        "tweag-jupyter.cachix.org-1:UtNH4Zs6hVUFpFBTLaA4ejYavPo5EFFqgd7G7FxGW9g="
+      ];
+    };
+  };
+}
