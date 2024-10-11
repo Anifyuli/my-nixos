dc5f8a1fe2b4b3a52a0e6df448a923006b3d6802
fmway <fm18lv@gmail.com>
2024-10-12 00:17:56 +0700

    (programs:fix) typo 🗿

--- programs/games/default.nix
+++ programs/games/default.nix
@@ -6,8 +6,8 @@
   ];
 
   # required for steam gamescope
-  programs.gamescope = {
-    enable = lib.mkIf config.programs.steam.enable true;
-    capSysNice.enable = lib.mkIf config.programs.steam.enable true;
+  programs.gamescope = lib.mkIf config.programs.steam.enable {
+    enable = true;
+    capSysNice = true;
   };
 }
