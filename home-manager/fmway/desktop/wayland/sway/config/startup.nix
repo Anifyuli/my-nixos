d4eba97d002abdbc3f8a3f2d1d78f803d37475e3
fmway <fm18lv@gmail.com>
2024-10-14 23:26:34 +0700

    (home-manager:fix) sway env

--- home-manager/fmway/desktop/wayland/sway/config/startup.nix
+++ home-manager/fmway/desktop/wayland/sway/config/startup.nix
@@ -5,6 +5,8 @@ in [
   { command = com "foot" "--server"; }
   { command = com "light" "-S 5%"; }
   { command = ''wl-paste -t text --watch ${com "clipman" "store --no-persist"}''; }
+  { command = "systemctl --user import-environment XDG_CURRENT_DESKTOP XDG_SESSION_TYPE"; }
+  { command = "dbus-update-activation-environment WAYLAND_DISPLAY"; }
   {
     command = com "swayidle" ''w timeout 300 '${lock} -f -c 000000' timeout 600 'swaymsg "output * power off"' resume 'swaymsg "output * power on"' before-sleep "${lock} -f -c 000000"'';
   }
