d507efd96a207403d8805545203e035b05a61494
fmway <fm18lv@gmail.com>
2024-10-14 23:22:44 +0700

    (config:fix) xdg

--- systems/xdg.nix
+++ systems/xdg.nix
@@ -1,10 +1,11 @@
+{ pkgs, ... }:
 {
   # portal with wayland support
   portal = {
     enable = true;
-    # extraPortals = with pkgs; [
-      # xdg-desktop-portal-wlr
-    #  xdg-desktop-portal-gtk
-    #];
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
   };
 }
