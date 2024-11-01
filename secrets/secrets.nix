ea030592a4759e9980b4845a84ec601de8903c94
fmway <fm18lv@gmail.com>
2024-11-01 20:21:11 +0700

    (secrets:fix) simplification

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,21 +3,10 @@ let
     system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO58iqlTtlgkt9e8u7X6hbBusv6yf9LGJGk16/YhVDOw";
     fmway = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPyjA20DdCiceuM3w328rtf8yv1FkawYhO/4zP7LWarc";
   };
-
-  inherit (builtins)
-    foldl'
-    hasAttr
-    ;
   
-  mySecret = arr: foldl' (acc: name: let
-    file = "${name}.age";
-    publicKeys = 
-      if name != "system" && hasAttr name keys then
-        [ keys.system keys.${name} ]
-      else
-        [ keys.system ];
-  in {
-    "${file}".publicKeys = publicKeys;
+  mySecret = arr: builtins.foldl' (acc: name: {
+    "${name}.age".publicKeys = [ keys.system ] ++
+      (if name != "system" && name ? ${keys} then [ keys.${name} ] else []);
   } // acc) {} arr;
 
 in mySecret [ 
