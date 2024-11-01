1c813e579a2337fe65c0b762e7482019668521f0
fmway <fm18lv@gmail.com>
2024-11-01 20:18:43 +0700

    (nothing) ...

--- home-manager/fmway/dconf.nix
+++ home-manager/fmway/dconf.nix
@@ -95,8 +95,7 @@ in {
     {
       name = "increment cursor size";
       binding = "<Alt><Super>equal";
-      command = bash "increment-cursor" # sh
-      ''
+      command = bash "increment-cursor" /* sh */ ''
         CURRENT=$(gsettings get org.gnome.desktop.interface cursor-size)
         gsettings set org.gnome.desktop.interface cursor-size $(( CURRENT + 1 ))
       '';
@@ -104,8 +103,7 @@ in {
     {
       name = "decrement cursor size";
       binding = "<Alt><Super>minus";
-      command = bash "decrement-cursor" # sh
-      ''
+      command = bash "decrement-cursor" /* sh */ ''
         CURRENT=$(gsettings get org.gnome.desktop.interface cursor-size)
         [ ! -z $CURRENT ] &&
           [ $CURRENT -ge 1 ] &&
