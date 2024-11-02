0b204ee85a25ec9ad7e0c0e4d054b351da57bf99
fmway <fm18lv@gmail.com>
2024-11-02 23:16:05 +0700

    (home-manager:tmux:add) add some stuff

--- home-manager/fmway/programs/tmux/default.nix
+++ home-manager/fmway/programs/tmux/default.nix
@@ -4,6 +4,10 @@
   baseIndex = 1;
   clock24 = true;
   extraConfig = extra.tmux;
+  escapeTime = 0;
+  mouse = true; # mouse support
+  newSession = true; # create new session when attack and none
+  terminal = "screen-256color";
   customPaneNavigationAndResize = true; # Override the hjkl and HJKL bindings for pane navigation and resizing in VI mode.
   plugins = with pkgs.tmuxPlugins; [
     {
