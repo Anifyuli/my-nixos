1d6026eabb56fd410e07f87c973ff0512b765f5c
fmway <fm18lv@gmail.com>
2024-11-20 08:39:06 +0700

    (secrets:fix) add conditions for permission

--- secrets/default.nix
+++ secrets/default.nix
@@ -15,11 +15,17 @@ let
   # all /etc/nixos/secrets/<file>.age will be imported to age.secrets.<file>
   age.secrets = listToAttrs (map (file: rec {
     name = getNameAge file;
-    value = {
+    value = let
+      isOwnedUser = length resultAges.${file}.publicKeys > 1;
+    in {
       file = folder + ("/" + file);
       path = "/etc/secrets/${name}";
-      owner = lib.mkIf (length resultAges.${file}.publicKeys > 1) name;
-      mode = "0662"; # rw-rw-r--
+      owner = lib.mkIf isOwnedUser name;
+      mode =
+        if isOwnedUser then
+          "0660"  # rw-rw----
+        else
+          "0662"; # rw-rw-r--
       symlink = false;
     };
   }) files);
